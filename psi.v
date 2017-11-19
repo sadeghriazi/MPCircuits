e2141f7ca1aa9fa2a422bf6aae2cebc6d16711a4
Siam Umar Hussain <siamumar@ucsd.edu>
2017-11-19 09:05:22 -0800

    Update psi.v

--- psi.v
+++ psi.v
@@ -17,8 +17,8 @@ wire [n*b-b-1:0] con_wire; // Connection wire
 assign con_wire [b-1:0] = all_input [b-1:0] & all_input [2*b-1:b];
 genvar i; 
 generate 
-	for (i=1 ; i<n-1; i++) begin: main_conn
-		assign con_wire [(i+1)*b-1:i*b] = con_wire [i*b-1:(i-1)*b] & all_input [(i+2)*b-1:(i+1)*b]; 
+    for (i=1 ; i<n-1; i++) begin: main_conn
+	assign con_wire [(i+1)*b-1:i*b] = con_wire [i*b-1:(i-1)*b] & all_input [(i+2)*b-1:(i+1)*b]; 
     end
 endgenerate 
 
